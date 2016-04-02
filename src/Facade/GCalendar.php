bd3f8dcece48f70f82bb02026fd7a852d7d4b115
szabomikierno <szabomikierno@gmail.com>
2016-04-02 03:09:42 +0300

    Fixed Facade

--- src/Facade/GCalendar.php
+++ src/Facade/GCalendar.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Facade;
  
 class GCalendar extends Facade {
  
-    protected static function getFacadeAccessor() { return 'googlecalendar'; }
+    protected static function getFacadeAccessor() { return 'Szabomikierno\GoogleCalendarLaravelWrapper\GoogleCalendar'; }
  
 }
